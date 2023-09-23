d9fb1117d36267dd9d85c79a44a6187f946e4134
programmerasif <mdasifkhan3757@gmail.com>
2023-09-23 17:30:45 +0600

    added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Card from "./Card/Card"
+
+function App() {
+
+
+  return (
+    <>
+    <div className="bg-[#496ba1] h-[60vh] text-white text-center">
+      <h5 className="text-3xl font-bold pt-24">Chose Your Plan</h5>
+      <p className="text-xl">European data service provide lots of play to all type coustomer can get a owne server an it ensure high quelity to coustomer</p>
+    </div>
+ 
+    <Card />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Card/Card.jsx
@@ -0,0 +1,73 @@
+import { FaBicycle, FaCheck } from 'react-icons/fa';
+const Card = () => {
+    return (
+        <div className="grid absolute top-56 grid-cols-3 wf justify-center items-center w-[80%] left-40  gap-14">
+            
+            <div className=" text-3xl font-bold bg-white w-[80%] mx-auto rounded-md drop-shadow-2xl p-5">
+
+            <div className='flex justify-center items-center flex-col'>
+                <h5>Start-Up</h5>
+                <FaBicycle  className='text-8xl text-[#5a5679] '/>
+                <h5 className='text-[#5a5679] '>Free</h5>
+                
+            </div>
+                 <h5 className='text-xl text-center mt-8 pt-5 text-[#5a5679] border-t-2 border-gray-200'>Enjoy All Test Feauter</h5>
+
+                 <div className='text-sm flex justify-start items-start flex-col gap-4 mt-5'>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  Intel i3-7100, 4 Threads, 3.90 GHz Turbo, 3MB Cache</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  Min 8 GB / Max 32 GB RAM</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  1Gbps Shared Bandwidth</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  2x Disk Drives</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  OS Your choice</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  Software RAID</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  No KVM</span>
+                 </div>
+                 <button className='bg-[#5a5679] px-8 py-3 text-xl w-full text-white mt-3 rounded-sm'>Subscribe</button>
+            </div>
+            <div className=" text-3xl font-bold bg-white w-[80%] mx-auto rounded-md drop-shadow-2xl p-5">
+
+            <div className='flex justify-center items-center flex-col'>
+                <h5>Start-Up</h5>
+                <FaBicycle  className='text-8xl text-[#5a5679] '/>
+                <h5 className='text-[#5a5679] '>Free</h5>
+                
+            </div>
+                 <h5 className='text-xl text-center mt-8 pt-5 text-[#5a5679] border-t-2 border-gray-200'>Enjoy All Test Feauter</h5>
+
+                 <div className='text-sm flex justify-start items-start flex-col gap-4 mt-5'>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  Intel i3-7100, 4 Threads, 3.90 GHz Turbo, 3MB Cache</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  Min 8 GB / Max 32 GB RAM</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  1Gbps Shared Bandwidth</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  2x Disk Drives</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  OS Your choice</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  Software RAID</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  No KVM</span>
+                 </div>
+                 <button className='bg-[#5a5679] px-8 py-3 text-xl w-full text-white mt-3 rounded-sm'>Subscribe</button>
+            </div>
+            <div className=" text-3xl font-bold bg-white w-[80%] mx-auto rounded-md drop-shadow-2xl p-5">
+
+            <div className='flex justify-center items-center flex-col'>
+                <h5>Start-Up</h5>
+                <FaBicycle  className='text-8xl text-[#5a5679] '/>
+                <h5 className='text-[#5a5679] '>Free</h5>
+                
+            </div>
+                 <h5 className='text-xl text-center mt-8 pt-5 text-[#5a5679] border-t-2 border-gray-200'>Enjoy All Test Feauter</h5>
+
+                 <div className='text-sm flex justify-start items-start flex-col gap-4 mt-5'>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  Intel i3-7100, 4 Threads, 3.90 GHz Turbo, 3MB Cache</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  Min 8 GB / Max 32 GB RAM</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  1Gbps Shared Bandwidth</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  2x Disk Drives</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  OS Your choice</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  Software RAID</span>
+                    <span className=' flex justify-self-center items-center gap-3'> <span className='text-green-500 '> <FaCheck /></span>  No KVM</span>
+                 </div>
+                 <button className='bg-[#5a5679] px-8 py-3 text-xl w-full text-white mt-3 rounded-sm'>Subscribe</button>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
